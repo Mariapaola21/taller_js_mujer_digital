c42adf63ef5590cb36a80fe4477de4bf63c9f70d
Mariapaola21 <pa4742621@gmail.com>
2024-08-27 07:11:28 -0500

    Taller_calificable

---
+++ taller_1.js
@@ -0,0 +1,33 @@
+let temperatura = 25;
+if(temperatura > 30 ){
+    console.log ("Hace calor");
+} else{
+    console.log("Hace frio")
+} 
+
+
+for(let i = 1; i <=10; i++){
+    console.log(i)
+}
+
+
+let colores = ["Negro", "Morado", "Blanco"];
+console.log(colores[0]);
+
+
+function suma (a,b) {
+    return a+b;
+    
+} console.log (suma (5,5));
+
+const libro = {
+Titulo:"cien años de soledad",  
+Autor: "Gabriel Garcia Marquez", 
+Año_de_publicación: 1967
+
+}; console.log (libro)
+
+ 
+
+
+
